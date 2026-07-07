35eaa0bea7a35e00014ffc0402f3f6e13cb1ce12
mkosmala <marcin.kosmala@deviniti.com>
2026-07-07 20:58:03 +0200

    feat: add mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -5,6 +7,8 @@ import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>
 );
